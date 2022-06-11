290b1aee5e07b75920299beb194933f575b1b16d
311Volt <113amper@gmail.com>
2022-06-11 13:04:41 +0200

    change Random to only use OS-provided facilities

--- src/crypto/Random.cpp
+++ src/crypto/Random.cpp
@@ -1,111 +1,62 @@
 
-#include <cstring>
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <random>
-
 #include "Random.hpp"
-#include "CryptoObjects.h"
+#include <stdio.h>
 
-Random randomInstance;
-thread_local const uint32_t thread_id = ++randomInstance.threadCounter;
-thread_local uint64_t threadLocalCounter;
-thread_local std::random_device stdRandom;
-
-
-Random::State::State() noexcept {
-}
-
-void Random::State::XorTimeRandom() {
-	union {
-		time_t t = time(NULL);
-		uint32_t some[(sizeof(time_t)+3)/4];
-	};
-	for(int i=0; i<sizeof(some)/4; ++i)
-		m[i] ^= some[i];
-	m[15] ^= thread_id;
-	*((uint64_t*)&(m[5])) ^= rand();
-	*((uint64_t*)&(m[6])) ^= stdRandom();
-	*((uint64_t*)&(m[8])) ^= ++randomInstance.nonUatomicBetweenThreadsUsageCounter;
-	*((decltype(clock())*)&(m[10])) ^= clock();
-	*((uint64_t*)&(m[12])) ^= ++threadLocalCounter;
-	*((uint64_t*)&(m[14])) ^= ++randomInstance.atomicUsageCounter;
-}
+#include <memory>
+#include <stdexcept>
 
 #ifdef __unix__
-#include <sys/random.h>
-#endif
 
-void Random::State::XorWithdevrandom() {
-#ifdef __unix__
-	union {
-		uint8_t r[64];
-		uint32_t r32[16];
-	};
-	
-	getrandom(r, 64, GRND_RANDOM);
-	for(int i=0; i<16; ++i)
-		m[i] ^= r32[i];
-	
-	FILE* devrandom = fopen("/dev/random", "r");
-	if(devrandom) {
-		fread(r, 1, 64, devrandom);
-		for(int i=0; i<16; ++i)
-			m[i] ^= r32[i];
+struct FileCloser{void operator()(FILE* f){fclose(f);}};
+
+void Random::Fill(void* ptr, size_t size) 
+{
+	std::unique_ptr<FILE, FileCloser> f(fopen("/dev/urandom", "rb"));
+	if(!f) {
+		throw std::runtime_error("cannot open /dev/urandom");
 	}
-	
-	memset(r, 0, 64);
-#endif
-}
-
-Random::State::~State() {
-	memset(m, 0, sizeof(State));
-}
-
-Random::State& Random::State::operator^=(const Random::State& other) {
-	for(int i=0; i<16; ++i) {
-		uint32_t v = __atomic_xor_fetch((uint32_t*)other.m+i, 0, 0);
-		__atomic_xor_fetch(m+i, v, 0);
-	}
-	return *this;
+	fread(ptr, 1, size, f.get());
 }
 
 
-Random::Random() {
-}
+#elif defined(WIN32)
 
-Random::~Random() {
-}
+//****NOT TESTED****
 
-void Random::Fill(void* ptr, size_t size) {
-	State state;
-	state ^= randomInstance.state;
-	state.XorTimeRandom();
-	state.XorWithdevrandom();
-	Chacha20Block(state.m);
-	
-	uint32_t* ptr32 = (uint32_t*)ptr;
-	size_t fullBlocks = size/(16*4);
-	for(size_t i=0; i<fullBlocks; ++i, ptr32+=16, size-=16*4) {
-		for(int j=0; j<16; ++j) {
-			ptr32[i] ^= state.m[i];
+#include <windows.h>
+#include <Wincrypt.h>
+
+struct RandomContext {
+	HCRYPTPROV hCryptProv;
+
+	RandomContext()
+	{
+		auto res = CryptAcquireContextA(
+			&hCryptProv,
+			NULL,
+			"Microsoft Base Cryptographic Provider v1.0",
+			PROV_RSA_FULL,
+			CRYPT_VERIFYCONTEXT
+		);
+
+		if(!res) {
+			throw std::runtime_error("cannot acquire Win32 cryptographic context");
 		}
-		state.XorTimeRandom();
-		Chacha20Block(state.m);
-	}
-	uint8_t* ptr8 = (uint8_t*)ptr32;
-	size_t rest = size - fullBlocks*16*4;
-	if(rest > 0) {
-		uint8_t* state_ptr = (uint8_t*)state.m;
-		for(size_t i=0; i<rest; ++i) {
-			ptr8[i] ^= state_ptr[i];
-		}
-		state.XorTimeRandom();
-		Chacha20Block(state.m);
 	}
 
-	randomInstance.state ^= state;
+	~RandomContext()
+	{
+		CryptReleaseContext(hCryptProv, 0);
+	}
+};
+
+void Random::Fill(void* ptr, size_t size)
+{
+	static RandomContext ctx;
+
+	CryptGenRandom(ctx.hCryptProv, size, ptr);
 }
 
+#else
+	#error "sorry, not implemented"
+#endif
